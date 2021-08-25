2c79cdf5817b94af4cc815617c95b3b9c966ca4b
Samuel Leßmann <[samuel@LMpost.de]>
2021-08-25 18:21:55 +0200

    Schöner Graph.

--- main.py
+++ main.py
@@ -2,9 +2,7 @@ from diceGame.diceGameOptimizer import optimizeDiceGame
 from diceGame.output import generatePlot
 
 import random
-random.seed(42)
 import numpy
-numpy.random.seed(42)
 
 EVO_DEFAULT = {"strategyRep": 0, "populationSize": 100, "chooseBest": 0.2, "changeRate": 1, 
 "generations": 50, "fitnessAgainst": "all"}
@@ -14,12 +12,16 @@ RL_DEFAULT = {"strategyRep": 0, "alpha": 0.9, "gamma": 0.5, "epsilon": 0.3, "eps
 
 def main():
     evo_reps = []
-    evo_kwargs = {"strategyRep": 0, "populationSize" : 20, "generations": 125}
-    for i in range(3,4):
+    evo_kwargs = {"strategyRep": 0, "populationSize" : 200, "generations": 50, "fitnessAgainst": 0.8}
+    for i in range(0,4):
+        random.seed(i)
+        numpy.random.seed(i)
         evo_kwargs["strategyRep"] = i
         evo_reps.append(optimizeDiceGame(6, 3, "EVO", evo_kwargs))
     #print(rewards)
     generatePlot(evo_reps)
+
+        
 
 
 
