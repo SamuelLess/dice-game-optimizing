fa9d2daeaf35c3fdfa0b97626a30833e84165f5f
SamuelLess <[samuel@LMpost.de]>
2021-09-01 17:48:13 +0200

    Output neu organisiert. Sammelt testRuns mit gleichem Namen und berechnet & plottet min/max/mean.

--- diceGameOptimizing/output.py
+++ diceGameOptimizing/output.py
@@ -2,15 +2,54 @@ import matplotlib.pyplot as plt
 
 def generatePlot(testRuns: (str, [(int, float)])):
 	#print(testRuns)
+
+	names = {run[0] for run in testRuns}
+
+	nameAssignedRewards = {}
+	for name, rewards in testRuns:
+		nameAssignedRewards.setdefault(name, []).append(rewards[1::])
+	
+	combinedRuns = {name: evalSameNameTestRuns(nameAssignedRewards[name]) 
+					for name in names}
+	
 	plt.xlabel("Ausgewertete Spiele")
 	plt.ylabel("Fitness")
 	#plt.gca().set_ylim([0.5,0.65])
-
-	plt.title("Vergleich von Strategieoptimierungsalgorithmen")
-	#TODO: wenn mehrere mit **gleichem Namen**, dann als Wiederholungen sammeln und Varianz/Abweichung einzeichenen
-	for name, rewards in testRuns:
-		x, y = zip(*rewards[1::])
-		plt.plot(x, y, label=name, linewidth=1)
-		print(f"{name}: {str(rewards[-1])} rewardPoints: {len(rewards)}")
+	for name in names:
+		meanRewardPoints = combinedRuns[name]["mean"]
+		x, ymean = zip(*meanRewardPoints)
+		plt.plot(x, ymean, label=name, linewidth=1)
+		plt.fill_between(x, combinedRuns[name]["min"], 
+							combinedRuns[name]["max"], alpha=0.5)
+		print(f"{name}: {str(rewards[-1])} rewardPoints: {len(rewards)} * {len(nameAssignedRewards[name])}")
+	
 	plt.legend()
 	plt.show()
+
+def evalSameNameTestRuns(testRunRewardPoints: [(str, float)]):
+	rewardPoints = {"mean": None, "min": None, "max": None}
+	rewardPoints["mean"] = meanlists(testRunRewardPoints)
+	rewardPoints["min"] = minlists(testRunRewardPoints)
+	rewardPoints["max"] = maxlists(testRunRewardPoints)
+	return rewardPoints
+
+def minlists(lists):
+	mins = []
+	for row in zip(*lists):
+		rowVals = [item[1] for item in row]
+		mins.append( min(rowVals))
+	return mins
+
+def maxlists(lists):
+	maxs = []
+	for row in zip(*lists):
+		rowVals = [item[1] for item in row]
+		maxs.append(max(rowVals))
+	return maxs
+
+def meanlists(lists):
+	means = []
+	for row in zip(*lists):
+		rowVals = [item[1] for item in row]
+		means.append((row[0][0], sum(rowVals)/float(len(rowVals))))
+	return means
