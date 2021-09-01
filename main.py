dab0f377b86f09cef459c12751d9a136f596925b
SamuelLess <[samuel@LMpost.de]>
2021-09-01 17:48:45 +0200

    Lesbarkeit.

--- main.py
+++ main.py
@@ -8,30 +8,39 @@ EVO_DEFAULT = {"strategyRep": 0, "populationSize": 100, "chooseBest": 0.2, "chan
 "generations": 50, "fitnessAgainst": "all"}
 
 RL_DEFAULT = {"strategyRep": 0, "defaultQValue": 0.5, "alpha": 0.9, "gamma": 0.5, 
-"epsilon": 0.3, "epsilonDecay": 0.999, "timeSteps": 100}
+"epsilon": 0.3, "endEpsilon": 0.01, "epsilonDecay": None, "timeSteps": 100, "rewardPointDensity": 0.001}
 
 PIPS = 6
 SIDES = 3
 
+runPerRepEvo = 2
+
+runsRl = 10
+    
+evo_kwargs = {"strategyRep": 0, "populationSize" : 50, "generations": 50, 
+    "fitnessAgainst": "all"}
+
+rl_kwargs = {"defaultQValue": float(0.0) ,"alpha": 0.5, "gamma": 0.7, 
+    "epsilon": 0.8, "endEpsilon": 0.01, "epsilonDecay": 0.9995, 
+    "timeSteps": 70000, "rewardPointDensity": 0.0005}
+
+playEvoReps = [0]
+
 def main():
     print(f"({PIPS=}, {SIDES=})")
     reps = []
-    evo_kwargs = {"strategyRep": 0, "populationSize" : 100, "generations": 25, "fitnessAgainst": "all"}
-    for i in range(0,4):
+    for repNum in playEvoReps:
+        evo_kwargs["strategyRep"] = repNum
+        random.seed(repNum)
+        numpy.random.seed(repNum)
+        for _ in range(runPerRepEvo):
+            reps.append(optimizeDiceGame(PIPS, SIDES, "EVO", evo_kwargs=evo_kwargs))
+
+    for i in range(runsRl):
         random.seed(i)
         numpy.random.seed(i)
-        evo_kwargs["strategyRep"] = i
-        #reps.append(optimizeDiceGame(PIPS, SIDES, "EVO", evo_kwargs=evo_kwargs))
+        reps.append(optimizeDiceGame(PIPS, SIDES, "RL", rl_kwargs=rl_kwargs, output = False))
 
-    
-    rl_kwargs = {"defaultQValue": float(0.0) ,"alpha": 0.5, "gamma": 0.7, 
-    "epsilon": 0.8, "epsilonDecay": 0.9995, "timeSteps": 1000, "rewardPointDensity": 0.4}
-    end_epsilon = 0.01
-    rl_kwargs["epsilonDecay"] = (end_epsilon/rl_kwargs["epsilon"])**(1.0/rl_kwargs["timeSteps"])
-    
-    random.seed(42)
-    numpy.random.seed(42)
-    reps.append(optimizeDiceGame(PIPS, SIDES, "RL", rl_kwargs=rl_kwargs, output = False))
 
     generatePlot(reps)
 
