319139123092504b9b9b816606e8b04602b1e177
SamuelLess <[samuel@LMpost.de]>
2021-10-08 17:41:36 +0200

    Einfügen der Namen für Label für TestRuns.

--- diceGameOptimizing/diceGameOptimizer.py
+++ diceGameOptimizing/diceGameOptimizer.py
@@ -2,7 +2,7 @@ from diceGameOptimizing.diceGame import Game
 from diceGameOptimizing.evolutionary.evolutionarySearch import EvolutionarySearch
 from diceGameOptimizing.reinforcement.reinforcementLearning import ReinforcementLearning
 
-def optimizeDiceGame(pips, sides, optimizerType, evo_kwargs=None, rl_kwargs=None, output=False):
+def optimizeDiceGame(pips, sides, optimizerType, newname=None, evo_kwargs=None, rl_kwargs=None, output=False):
     """
     pips, sides: p, s
     
@@ -16,12 +16,12 @@ def optimizeDiceGame(pips, sides, optimizerType, evo_kwargs=None, rl_kwargs=None
     if optimizerType == "EVO":
         evo = EvolutionarySearch(Game(pips, sides), **evo_kwargs, output=output)
         rewardPoints = evo.train()
-        name = "EVO_" + str(evo_kwargs.get("strategyRep", "0"))
+        name = newname if newname is not None else optimizerType
 
     if optimizerType == "RL":
         rl = ReinforcementLearning(Game(pips, sides), **rl_kwargs, output=output)
         rewardPoints = rl.train()
-        name = "RL"
+        name = newname if newname is not None else optimizerType
         #name = ("RL_" + "dqv" + str(rl_kwargs["defaultQValue"]) + "α" + str(rl_kwargs["alpha"]) +
         #    "_γ" + str(rl_kwargs["gamma"]) + "_ε" + str(rl_kwargs["epsilon"]))
 
